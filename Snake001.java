635b39e522a550f31821fd3d2a1f96db77e064ad
Ilya Zakharchenko <elaidja@gmail.com>
2016-10-17 19:39:55 +0300

    The Snake ver 0.1 is done. Snake moves, grows. Game ends if snake eats itself, gets out of map (lose) or if snake fills every cell of the map(win)

---
+++ Snake001.java
@@ -0,0 +1,191 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Snake001 {
+	
+	public static void main(String [] args) {
+
+		int length = 1; // кол-во секций
+		int [] sectionY = new int [size() * size() - 1];
+		int [] sectionX = new int [size() * size() - 1];
+		char [][] map = new char [size()][size()]; // поле size x size
+		int headX = size() / 2, headY = size() / 2; // начальные координаты головы
+		char space = '.'; // пустая ячейка
+		sectionX[0] = size() / 2 + 1;
+		sectionY[0] = size() / 2; // координаты секции змеи
+		char section = '*'; // секция змеи
+		for (int i = 0; i < size(); i++) {
+			for (int j = 0; j < size(); j++) {
+				map[i][j] = space;
+			}
+		} // заполнение поля
+		map[headY][headX] = '>'; // размещение головы
+		map[sectionY[0]][sectionX[0]] = section;
+		fruit(map);
+		for (int i = 0; i < size(); i++) {
+			for (int j = 0; j < size(); j++) {
+				System.out.print(map[i][j]);
+				System.out.print(" ");
+			}	
+			System.out.println();
+		} // вывод поля
+		char p;
+		boolean alive = true, win = false;
+		while (alive && !win) {
+			Scanner sc = new Scanner(System.in);
+			p = sc.next().charAt(0);	
+			if (p == 'a') {
+				if (map[headY][headX] == '<') {
+					continue;					
+				}
+				else if (map[headY][headX - 1] == section) {
+					alive = false;
+					break;
+				}
+				else if (map[headY][headX - 1] == space) {
+					map[sectionY[0]][sectionX[0]] = space;
+					move(sectionX, sectionY, length, headX, headY);
+					map[headY][headX] = section;
+					headX--;
+					map[headY][headX] = '>';
+				}
+				else if (map[headY][headX - 1] == '@') {
+					length++;
+					growth(sectionX, sectionY, headX, headY, length);
+					map[headY][headX] = section;
+					headX--;
+					map[headY][headX] = '>';
+					if (length == size() * size() - 1) {
+						win = true;
+						break;
+					}
+					fruit(map);
+				}
+			}
+			if(p == 'w') {
+				if (map[headY][headX] == '^') {
+					continue;			
+				}
+				else if (map[headY - 1][headX] == section) {
+					alive = false;
+					break;
+				}
+				else if (map[headY - 1][headX] == space) {
+					map[sectionY[0]][sectionX[0]] = space;
+					move(sectionX, sectionY, length, headX, headY);
+					map[headY][headX] = section;
+					headY--;
+					map[headY][headX] = 'v';
+				}
+				else if (map[headY - 1][headX] == '@') {
+					length++;
+					growth(sectionX, sectionY, headX, headY, length);
+					map[headY][headX] = section;
+					headY--;
+					map[headY][headX] = 'v';
+					if (length == size() * size() - 1) {
+						win = true;
+						break;
+					}
+					fruit(map);
+				}
+			}
+			if(p == 's') {
+				if (map[headY][headX] == 'v') {
+					continue;			
+				}
+				else if (map[headY + 1][headX] == section) {
+					alive = false;
+					break;
+				}
+				else if (map[headY + 1][headX] == space) {
+					map[sectionY[0]][sectionX[0]] = space;
+					move(sectionX, sectionY, length, headX, headY);
+					map[headY][headX] = section;
+					headY++;
+					map[headY][headX] = '^';
+					System.out.println("move");
+				}
+				else if (map[headY + 1][headX] == '@') {
+					length++;
+					growth(sectionX, sectionY, headX, headY, length);
+					map[headY][headX] = section;
+					headY++;
+					map[headY][headX] = '^';
+					if (length == size() * size() - 1) {
+						win = true;
+						break;
+					}
+					fruit(map);
+				}
+			}	
+			if(p == 'd') {
+				if (map[headY][headX] == '>') {
+					continue;
+				}
+				else if (map[headY][headX + 1] == section) {
+					alive = false;
+					break;
+				}
+				else if (map[headY][headX + 1] == space) {
+					map[sectionY[0]][sectionX[0]] = space;
+					move(sectionX, sectionY, length, headX, headY);
+					map[headY][headX] = section;
+					headX++;
+					map[headY][headX] = '<';
+				}
+				else if (map[headY][headX + 1] == '@') {
+					length++;
+					growth(sectionX, sectionY, headX, headY, length);
+					map[headY][headX] = section;
+					headX++;
+					map[headY][headX] = '<';
+					if (length == size() * size() - 1) {
+						win = true;
+						break;
+					}
+					fruit(map);
+				}
+			}				
+			for (int i = 0; i < size(); i++) {
+				for (int j = 0; j < size(); j++) {
+					System.out.print(map[i][j]);
+					System.out.print(" ");
+				}	
+				System.out.println();
+			}
+		}
+		if (!alive) {
+			System.out.println("The snake ate itself and died in agony. Game over.");
+		}
+		else if (win) {
+			System.out.println("You won! Congratulations! You can undoubtedly live the snake life!");
+		}
+	}
+	public static void fruit(char[][] map) {
+		Random r = new Random();
+		int y, x;
+		do {
+			y = r.nextInt(size());
+			x = r.nextInt(size());
+		}
+		while (map[y][x] != '.');
+		map[y][x] = '@';
+	} // метод для фрукта
+	public static int size() {
+		final int N = 5;
+		return N;
+	} // метод размера
+	public static void move(int[] sectionX, int[] sectionY, int length, int headX, int headY) {
+		for (int i = 0; i < length - 1; i++) {
+			sectionX[i] = sectionX[i + 1];
+			sectionY[i] = sectionY[i + 1];
+		}
+		sectionX[length - 1] = headX;
+		sectionY[length - 1] = headY;
+	} // метод для движения
+	public static void growth(int[] sectionX, int[] sectionY, int headX, int headY, int length) {
+		sectionX[length - 1] = headX;
+		sectionY[length - 1] = headY;
+	} // метод для роста
+}
